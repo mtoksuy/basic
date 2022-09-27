2053ad69206f7037882acdd494ececa550b6dc8c
mtoksuy <mtoksuy@programmerbox.com>
2022-09-27 23:27:30 +0900

    version_0.0 core.phpアップデート
    ・アクセスURL 重複スラッシュを1スラッシュに戻す
    ・$controller_query を作るための実装記述

--- index.php
+++ index.php
@@ -1,45 +1,39 @@
 <?php
 // core読み込み
 require_once('setting/core.php');
-//require_once('setting/config.php');
+require_once('setting/config.php');
 //require_once('setting/autoLoader.php');
+
+// アクセスURL 重複スラッシュを1スラッシュに戻す
+if(preg_match('/\/\//', $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'])) {
+	$FULL_HTTP = preg_replace('/\/\//', '/', FULL_HTTP);
+	header('Location: '.$FULL_HTTP);
+	exit;
+}
 
 // URLをスラッシュで分解
 $array_parse_uri = explode('/', $_SERVER['REQUEST_URI']);
-
-
-
-
-var_dump($array_parse_uri);
-
-
-
-
-$last_uri = end($array_parse_uri); // 最後の文字を取り出す
-$call = substr($last_uri, 0, strcspn($last_uri,'?'));   // クエリ文字列を外す
+//pre_var_dump($array_parse_uri);
+//pre_var_dump(ROOT_DIR);
+foreach($array_parse_uri as $kye => $value) {
+	if($value == ROOT_DIR || $value == '' || preg_match('/\?/', $value) == true) {
+//			pre_var_dump($value);
+	}
+		else {
+			$controller_query = $controller_query.'/'.$value;
+			pre_var_dump($value);
+		}
+}
+	pre_var_dump($controller_query);
+	pre_var_dump(PATH.'app/controller'.$controller_query.'/index.php');
 
 // app/controller/配下に同名のPHPファイルがないか探す。
-if(file_exists('../app/controller/' . $call . '.php')) {
-    // 見つかったファイルをインクルードしてコントローラーをインスタンス化
-    include('../app/controller/' . $call . '.php');
-    $class = 'app\controller\\' . $call;
-    $obj   = new $class();
-    
-    if($_SERVER["REQUEST_METHOD"] != "POST"){
-        // GETならindexメソッドを呼び出す
-        $response = $obj->index();
-    }else{
-        // POSTならpostメソッドを呼び出す
-        $response = $obj->post();
-    }
-
-    // コントローラーから戻された内容をレスポンスとして戻す。
-    echo $response;
-    exit;
-}else{
-    // ファイルがなければ404エラー
-    header("https/1.0 404 Not Found");
-    exit;
+if(file_exists('../app/controller'.$controller_query.'index.php')) {
+	// いまここ
 }
-
-echo 'a';
+	else {
+		pre_var_dump('エラー');
+		// ファイルがなければ404エラー
+		header("https/1.0 404 Not Found"); // あとで修正
+		exit;
+}
