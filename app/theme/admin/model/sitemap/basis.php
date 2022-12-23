11f7effafa000c84eb802580ecb44b03860b084c
mtoksuy <mtoksuy@programmerbox.com>
2022-12-23 09:45:02 +0900

    version_0.4 ページ作成時の重複バグ修正

--- app/theme/admin/model/sitemap/basis.php
+++ app/theme/admin/model/sitemap/basis.php
@@ -1,5 +1,27 @@
 <?php 
 class model_sitemap_basis {
+	//--------------------
+	// 全記事リスト取得
+	//--------------------
+	public static function article_all_list_get() {
+		$article_all_list_res = model_db::query("
+			SELECT *
+			FROM article
+			WHERE del = 0
+			ORDER BY primary_id DESC");
+		return $article_all_list_res;
+	}
+	//------------------
+	// pageリスト取得
+	//------------------
+	public static function page_all_list_get() {
+		$page_all_list_res = model_db::query("
+			SELECT *
+			FROM page
+			WHERE del = 0
+			ORDER BY primary_id ASC");
+		return $page_all_list_res;
+	}
 	//-------------------
 	//全記事リスト取得
 	//-------------------
