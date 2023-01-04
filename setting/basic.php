ff88c18eacb517415052781aed80ed2ad18005de
mtoksuy <mtoksuy@programmerbox.com>
2023-01-04 10:29:39 +0900

    version_0.6 [ページ機能]階層指定実装#32

--- setting/basic.php
+++ setting/basic.php
@@ -88,7 +88,7 @@ class basic {
 		
 		}
 			else {
-				if(mkdir($directory_path, 0755)) {
+				if(mkdir($directory_path, 0755, true)) {
 					chmod($directory_path, 0755);
 				}
 			}
@@ -126,7 +126,7 @@ class basic {
 				unlink($f);
 			} else {
 				// ディレクトリの場合（ファイルでない場合）は再度rmdirAll()を実行
-				Library_Dir_Basis::rmdirAll($f);
+				basic::rmdirAll($f);
 			}
 		} // foreach ($res as $f) {
 		// 中身を削除した後、本体削除
