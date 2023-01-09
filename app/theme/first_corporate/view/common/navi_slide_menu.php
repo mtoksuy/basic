8d8f3de0534f52eb0755a12f0f735a0e8ae55a79
mtoksuy <mtoksuy@programmerbox.com>
2023-01-09 16:51:58 +0900

    version_0.8 テーマ切り替え実装#18 first_corporate仮置き

---
+++ app/theme/first_corporate/view/common/navi_slide_menu.php
@@ -0,0 +1,34 @@
+
+		<div class="navi_slide_menu">
+			<ul>
+				<li<?php if(preg_match('/bbb/', $_SERVER['REQUEST_URI'], $array)) {echo ' class="now"';} ?>>
+					<a href="<?php echo HTTP; ?>/">Mission</a>
+				</li>
+
+				<li<?php if(preg_match('/bbb/', $_SERVER['REQUEST_URI'], $array)) {echo ' class="now"';} ?>>
+					<a href="<?php echo HTTP; ?>/">Project</a>
+				</li>
+
+				<li<?php if(preg_match('/bbb/', $_SERVER['REQUEST_URI'], $array)) {echo ' class="now"';} ?>>
+					<a href="<?php echo HTTP; ?>/">Service</a>
+				</li>
+
+				<li<?php if(preg_match('/bbb/', $_SERVER['REQUEST_URI'], $array)) {echo ' class="now"';} ?>>
+					<a href="<?php echo HTTP; ?>/">About</a>
+				</li>
+
+				<li<?php if(preg_match('/bbb/', $_SERVER['REQUEST_URI'], $array)) {echo ' class="now"';} ?>>
+					<a href="<?php echo HTTP; ?>/">Information</a>
+				</li>
+
+				<li<?php if(preg_match('/bbb/', $_SERVER['REQUEST_URI'], $array)) {echo ' class="now"';} ?>>
+					<a href="<?php echo HTTP; ?>/">Careers</a>
+				</li>
+
+				<li<?php if(preg_match('/bbb/', $_SERVER['REQUEST_URI'], $array)) {echo ' class="now"';} ?>>
+					<a href="<?php echo HTTP; ?>/">Contact</a>
+				</li>
+
+
+			</ul>
+		</div>
---
+++ app/theme/first_corporate/view/root/content.php
@@ -0,0 +1,137 @@
+<div class="root">
+	<div class="root_inner">
+
+	<div class="first_view">
+		<div class="first_view_title">クリエイターと、未来をかえる</div> 
+		<div class="first_view_bottom_title">Creator First, Creative Change.</div> 
+		<p>私たちは多くのパートナークリエイターと協力し、<br>
+	クリエイティブで企業や社会の課題解決を行うための<br>
+	プラットフォームを作る会社です。</p>
+	</div> <!-- first_view -->
+
+	<div class="content_block">
+		<div class="block_box">
+			<div class="left_block">
+				<h2>Mission</h2>
+				<div class="copy">
+					<span>クリエイターが</span>
+					<span>輝ける社会を</span>
+					<span>創造する</span>
+				</div>
+			</div>
+			<div class="right_block">
+				<p>私たちが目指していることは、「クリエイターが活躍できる環境と仕組み」を作ること。世界中のクリエイターが、自分のスキルや人生にマッチしたワークスタイルで、最高のパフォーマンスを発揮しながら人生を歩む。そして、素晴らしいクリエイティブが世の中を変えていく、そんな社会をかたちづくることです。</p>
+			</div>
+		</div>
+	</div> <!-- content_block -->
+
+
+
+
+
+
+
+
+	<div class="content_block_v2">
+		<h2>Service</h2>
+		<div class="copy">
+			<span>社会課題の解決に向け、 広告制作・サイト運用事業、 AdFlow事業、 プラットフォーム事業「thinc」の3つサービスを行っています。</span>
+		</div>
+		<div class="block_box">
+			<div class="left_block">
+				<img title="" alt="" src="<?php echo HTTP; ?>app/theme/<?php echo $site_data_array['theme']; ?>/assets/img/common/25286686_m.jpg">
+			</div>
+			<div class="right_block">
+				<h2>Service 01</h2>
+				<div class="copy">
+					<span>広告制作・サイト運用事業</span>
+				</div>
+				<p>ディスプレイ広告制作から、サイト保守・運用まで課題解決に向けた幅広いご提案が可能です。
+独自のクリエイターネットワークだからこそできる解決までのストーリーを共につくりあげます。</p>
+			</div>
+		</div> <!-- block_box -->
+
+		<div class="block_box">
+			<div class="left_block">
+				<h2>Service 01</h2>
+				<div class="copy">
+					<span>AdFlow事業</span>
+				</div>
+				<p>制作視点で生まれたクリエイティブ特化型
+工数削減ツール「AdFlow」
+クリエイティブに関わる全ての煩雑さを解消するためのサービスです。</p>
+			</div>
+			<div class="right_block">
+				<img title="" alt="" src="<?php echo HTTP; ?>app/theme/<?php echo $site_data_array['theme']; ?>/assets/img/common/25286686_m.jpg">
+			</div>
+		</div> <!-- block_box -->
+
+		<div class="block_box">
+			<div class="left_block">
+				<img title="" alt="" src="<?php echo HTTP; ?>app/theme/<?php echo $site_data_array['theme']; ?>/assets/img/common/25286686_m.jpg">
+			</div>
+			<div class="right_block">
+				<h2>Service 03</h2>
+				<div class="copy">
+					<span>「thinc」（シンク）プラットフォーム事業</span>
+				</div>
+				<p>「Think Creative」=「クリエイティブに考えよう」という意味を込めた「thinc」（シンク）というプラットフォームを展開しています。</p>
+			</div>
+		</div> <!-- block_box -->
+	</div> <!-- content_block -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<div class="card_article">
+			<div class="card_article_inner">
+					<?php echo $article_list_html; ?>
+					<?php echo $next_article_html; ?>
+			</div> <!-- card_article -->
+		</div> <!-- card_article_inner -->
+	</div> <!-- content_inner -->
+</div> <!-- content -->
