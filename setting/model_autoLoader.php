bfaa0c1b3e8a3f90c4a308b4d44792a6387168c9
mtoksuy <mtoksuy@programmerbox.com>
2023-03-23 21:06:32 +0900

    [プラグイン機能]新機能。仕様を変えないと実現できない#160

--- setting/model_autoLoader.php
+++ setting/model_autoLoader.php
@@ -6,6 +6,9 @@
 
 	$load_class_list = array();
 
+	////////////////////////////////////////////
+	// theme > admin
+	////////////////////////////////////////////
 	$cmd_2  = 'find '.PATH.'app/theme/admin/model -type f -name "*.php"';
 	exec($cmd_2, $arr_2, $res_2);
 	if($arr_2) {
@@ -36,6 +39,10 @@
 		// 現在設定しているテーマのモデル読み込み
 		$theme_name = $site_data_array['theme'];
 	}
+
+	////////////////////////////////////////////
+	// theme > $theme_name
+	////////////////////////////////////////////
 	$cmd = 'find '.PATH.'app/theme/'.$theme_name.'/model -type f -name "*.php"';
 	exec($cmd, $arr_1, $res_1);
 	if($arr_1) {
@@ -52,5 +59,31 @@
 			${$dir_data_under} = new $dir_data_under();
 		}
 	}
+	////////////////////////////////////////////
+	// plugin > 
+	////////////////////////////////////////////
+	$dir = PATH.'app/plugin';
+	foreach (glob("$dir/*", GLOB_ONLYDIR)  as $folder) {
+		$cmd_3  = 'find '.PATH.'app/plugin/'.basename($folder).'/model -type f -name "*.php"';
+		exec($cmd_3, $arr_3, $res_3);
+		if($arr_3) {
+			foreach($arr_3 as $key => $value) {
+				$value = str_replace(PATH.'app/plugin/'.basename($folder).'/model', '' , $value);
+				 preg_match('/^\/(.*)\//', $value, $value_array);
+				$dir_data = $value_array[0];
+				$file_name = str_replace($value_array[0], '' , $value);
+				$class_name = preg_replace('/\.php/', '' , $file_name);
+				require_once(PATH.'app/plugin/'.basename($folder).'/model'.$dir_data.$file_name);
+				$dir_data_under = preg_replace('/\//', '_' , $dir_data);
+				$dir_data_under =  'model'.$dir_data_under.$class_name;
+				$load_class_list[] = $dir_data_under;
+				${$dir_data_under} = new $dir_data_under();
+			}
+		}
+		// 初期化
+		$arr_3 = '';
+		$res_3 = '';
+	}
+
 // ソート逆
 $load_class_list = array_reverse($load_class_list);
