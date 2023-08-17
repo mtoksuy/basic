a45f8ba557ea2746d6c89500b2c49682f184c2a5
mtoksuy <mtoksuy@programmerbox.com>
2023-08-17 17:52:58 +0900

    version_0.9.9 [アドミン > 更新]更新がかかるディレクトリの権限を事前に調べて権限がおかしかったらメッセージを表示する#212

--- app/theme/admin/model/login/admin/coreupdate/basis.php
+++ app/theme/admin/model/login/admin/coreupdate/basis.php
@@ -53,4 +53,92 @@ class model_login_admin_coreupdate_basis {
 			basic::removeDir(PATH.'tmp');
 		}  // if($site_data_array['basic_version'] < $json_decode_response['latest_basic_version']) { 
 	}
+	//-------------------------------------
+	// ディレクトリの権限情報を取得する
+	//-------------------------------------
+	public static function dir_permission_info_get($directory) {
+		$dir_permission_info_data_array = array();
+		// ディレクトリが存在していたら権限情報を調べる
+		if(file_exists($directory)) {
+			// ディレクトリのパーミッションを取得
+			$permissions = fileperms($directory);
+			// パーミッションの数値を8進数から10進数に変換
+			$permissions = decoct($permissions);
+	
+			// 末尾の3桁のパーミッションを取得
+			$last_three_digits = $permissions % 1000;
+	
+
+
+/*************************************************************************/
+
+// ディレクトリの所有者のユーザーIDを取得する草案実装内容
+/*
+			// ディレクトリの所有者のユーザーIDを取得
+			$ownerId = fileowner($directory);
+			// ユーザーIDからユーザー名を取得
+			if(function_exists('posix_getpwuid')) {
+				$ownerInfo = posix_getpwuid($ownerId);
+				$ownerName = $ownerInfo['name'];
+			}
+			else {
+				// システムコマンドを使って所有者のユーザー名を取得します
+				$ownerName = trim(exec("stat -c '%U' {$directory}"));
+			}
+*/
+
+/*************************************************************************/
+
+	$os = strtoupper(substr(PHP_OS, 0, 3));
+
+	// windowsの場合
+	if ($os === 'WIN') {
+		// Windowsの場合の処理
+		exec("dir \"$directory\" 2>&1", $output, $returnVar);
+		if ($returnVar === 0) {
+			// Windowsの場合、ディレクトリの所有者情報は取得できないため、代わりにユーザー名を抽出
+			preg_match('/\[(.+)\]/', $output[5], $matches);
+			$ownerName = isset($matches[1]) ? $matches[1] : null;
+		}
+	}
+	// macの場合
+	elseif($os === 'DAR') {
+		// macOSの場合の処理
+		exec("ls -ld \"$directory\" 2>&1", $output, $returnVar);
+		if ($returnVar === 0) {
+			$parts = preg_split('/\s+/', $output[0]);
+			$ownerName = isset($parts[2]) ? $parts[2] : null;
+		}
+	}
+	// centosの場合
+	else {
+		// CentOSなどLinux系の場合の処理
+		exec("ls -ld \"$directory\" 2>&1", $output, $returnVar);
+		if ($returnVar === 0) {
+			$parts = preg_split('/\s+/', $output[0]);
+			$ownerName = isset($parts[2]) ? $parts[2] : null;
+		}
+	}
+
+
+/********************************************************************************/
+
+
+			$dir_permission_info_data_array['directory']             = $directory;
+			$dir_permission_info_data_array['permissions']        = $permissions;
+			$dir_permission_info_data_array['last_three_digits'] = $last_three_digits;
+			$dir_permission_info_data_array['ownerName']        = $ownerName;
+			$dir_permission_info_data_array['is_file_exists']       = true;
+		}
+		else {
+			$dir_permission_info_data_array['is_file_exists'] = false;
+		}
+		return $dir_permission_info_data_array;
+	}
+
+
+
+
+
+
 }
