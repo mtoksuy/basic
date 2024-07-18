ab74ecb3615da81509951c9e7d98612287052276
mtoksuy <mtoksuy@programmerbox.com>
2024-07-19 04:52:16 +0900

    PHPUnitのため修正

--- setting/db.php
+++ setting/db.php
@@ -4,7 +4,7 @@ class model_db {
 	// DB接続
 	//-------
 	public static function db_conect($db_config_array) {
-		$db = new mysqli($db_config_array['default']['connection']['hostname'], $db_config_array['default']['connection']['username'], $db_config_array['default']['connection']['password'], $db_config_array['default']['connection']['database']);
+		$db = new mysqli($db_config_array['default']['connection']['hostname'], $db_config_array['default']['connection']['username'], $db_config_array['default']['connection']['password'], $db_config_array['default']['connection']['database'], ini_get("mysqli.default_port"), ini_get("mysqli.default_socket"));
 		if ($db->connect_error) {
 			echo '<b>Warning</b>: mysqli::__construct(): ' . $db->connect_error;
 			return null; // 接続失敗時はnullを返す
@@ -18,6 +18,52 @@ class model_db {
 	//-------
 	public static function query($query) {
 		global $db_config_array;
+		require(PATH . 'setting/db_config.php');
+		$query_pattern = '';
+		// DB接続
+		$db = self::db_conect($db_config_array);
+		if (!$db) {
+			return []; // DB接続に失敗した場合は空の配列を返す
+		}
+
+		try {
+			if (preg_match('/INSERT INTO/', $query)) {
+				$query_pattern = 'INSERT';
+			} else if (preg_match('/SELECT/', $query)) {
+				$query_pattern = 'SELECT';
+			} else if (preg_match('/UPDATE/', $query)) {
+				$query_pattern = 'UPDATE';
+			}
+
+			$select_array = [];
+			if ($result = $db->query($query)) {
+				switch ($query_pattern) {
+					case 'SELECT':
+						while ($row = $result->fetch_assoc()) {
+							$select_array[] = $row;
+						}
+						$result->close();
+						break;
+					case 'INSERT':
+					case 'UPDATE':
+						$select_array = $result;
+						break;
+				}
+			}
+			return $select_array;
+		} catch (mysqli_sql_exception $e) {
+			// 後ほど logに出力する方法で再実装する
+			//			echo "SQL Error: " . $e->getMessage(); // エラーメッセージの出力
+			return []; // エラー発生時は空の配列を返す
+		}
+	}
+
+
+	//-------
+	// クエリ
+	//-------
+	public static function phpunitquery($query) {
+		require(PATH . 'setting/db_config.php');
 		$query_pattern = '';
 		// DB接続
 		$db = self::db_conect($db_config_array);
